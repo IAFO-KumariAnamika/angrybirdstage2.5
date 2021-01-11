b9044c87f8594559fa9343cedafe21ffbf42947a
IAFO-KumariAnamika <kumarianamika5798@gmail.com>
2021-01-11 12:46:31 +0530

    added size

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20);
-    platform=new Ground(180,305,300,170);
+    platform=new Ground(200,305,400,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
